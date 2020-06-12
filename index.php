de95fb4835a159b8ff8cfd23560cc611b89a6f75
ErolPehlivan98 <2pehle11@solent.ac.uk>
2020-06-12 21:15:32 +0100

    com2

--- index.php
+++ index.php
@@ -335,12 +335,13 @@
          		// Everything is loaded including images.
              	$(window).on("load", function(){
 
-             		// Render the page on modern browser only.
+
+								
              		if(renderPage) {
          		// Remove loader
                	$('body').addClass('loaded');
 
-               	
+
                	var allPages = $(".tm-section");
 
 
