0fc07563466778192962a567ca4fb659ca3982e4
ErolPehlivan98 <2pehle11@solent.ac.uk>
2020-06-13 22:29:34 +0100

    injection prevention for login

--- Login.php
+++ Login.php
@@ -1,7 +1,7 @@
 <?php
 
 session_start();
-
+$res2 =[null];
 
 $username = $_POST["username"];
 $password = $_POST["pass"];
@@ -9,9 +9,10 @@ $password = $_POST["pass"];
 $conn=new PDO("mysql:host=localhost;dbname=web", "root");
 
 
-$result=$conn->query("SELECT * FROM login WHERE username='$username' AND password='$password'");
-$row=$result->fetch();
-if($row==false)
+$result=$conn->query("SELECT * FROM login WHERE username=? AND password= ? ");
+$res2 = $conn->prepare($result);
+
+if($res2==false)
 {
 	echo "Incorrect username/password!";
 
@@ -19,6 +20,7 @@ if($row==false)
 else
 {
 
+$res2->execute(array($username, $password));
 	header("Location: index.php");
 	$_SESSION["login_key"] = "$username";
 }
